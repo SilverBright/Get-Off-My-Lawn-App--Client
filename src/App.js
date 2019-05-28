d8583e25193b8e7d9a9b61fa60a363da3743be58
github username <github email address>
2019-05-27 22:38:52 -0700

    add .env for rails api, stub out design for App.js

--- src/App.js
+++ src/App.js
@@ -3,10 +3,24 @@ import React, { Component } from 'react';
 class App extends Component {
   render() {
     return (
-      <div>
-        Get Off My Lawn
+      <div className="App">
+        <div className="navbar">
+          <h4>Navbar</h4> 
+          {/* <Navbar /> */}
+        </div>
+         <div className="ideas-app container">
+          <h1 className="center blue-text">Get Off My Lawn</h1>
+          <div>
+            Post
+          </div>
+          <div className="ideas collection">
+            add a post
+        </div>
+          {/* <Idea /> */}
+        </div>
       </div>
     );
   }
-};
+}
+
 export default App;
