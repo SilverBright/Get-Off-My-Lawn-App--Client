15f9c1bf5f4476565157c3770dac16576c10e6e7
github username <github email address>
2019-05-28 16:15:06 -0700

    stub out posts and content, fix css layout issues, delete comments

--- src/App.js
+++ src/App.js
@@ -2,8 +2,9 @@ import React, { Component } from 'react';
 import Posts from './components/Posts';
 
 let posts = [
-  { title: 'A Post Title', id:1, content: 'post contents', comments: 'post comments' },
-  { title: 'A Second Post Title', id:2, content: 'moar post contents', comments: 'moar post comments' }
+  { title: "Someone knocked on my door", id:1, content: "There were two of them!!!  Dressed nicely and holding clipboards. I called the police because they looked supicious.  You never know!!!"},
+  { title: "I think it's raining", id:2, content: "Just thought everyone should know in case you're going outside." },
+  { title: "Free rocks", id:3, content: "I got a pile of rocks on my drive...anyone needs any rocks? " }
 ]
 
 class App extends Component {
@@ -14,14 +15,14 @@ class App extends Component {
           <h4>Navbar</h4> 
           {/* <Navbar /> */}
         </div>
-         <div className="ideas-app container">
+         <div className="posts-app container">
           <h1 className="center green-text">Get Off My Lawn</h1>
           <div>
             Add a Post
           </div>
-          <div className="ideas collection">
+          {/* <div className="posts collection"> */}
             <Posts posts={posts}/>
-        </div>
+        {/* </div> */}
           {/* <Idea /> */}
         </div>
       </div>
--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,19 +1,15 @@
 // import React, { Component } from 'react';
 // import PostsAPI from './api/PostsAPI';
 import React from 'react';
-
-const Posts = ({ posts }) => {
-  const renderPosts = posts.map(post =>
-    <div className="brainstorm card" key={post.id}>
-      <div className="card-content">
-        <span className="card-title purple-text">{ post.title }</span>
-        {/* </Link> */}
-        <p>{ post.content }</p>
-        <br/>
-        {/* <button onClick={() => this.props.deleteIdea(idea.id)}>Delete Idea</button> */}
-      </div>
-    </div>
-  )
+  const Posts = ({ posts }) => {
+    const renderPosts = posts.map(post =>
+      <div className="post card" key={post.id}>
+        <div className="card-content">
+          <span className="card-title purple-text">{ post.title }</span>
+            <p>{ post.content }</p>
+          </div>
+        </div>
+      )
 
     return (
       <div>
