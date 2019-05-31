b131c20d7988dee52eeb0e62326ebb26f750c2fa
github username <github email address>
2019-05-31 16:56:04 -0700

    test out api fetch in App.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './App.css';
 
-class App extends Component {
+class App extends React.Component {
+
+  componentDidMount() {
+    fetch("http://localhost:3001/api/posts")
+      .then(r=>r.json())
+      .then(console.log)
+  }
+
   render() {
     return (
       <div>
         Hello World
       </div>
-    )
+    );
   }
 }
-
 
 export default App;
