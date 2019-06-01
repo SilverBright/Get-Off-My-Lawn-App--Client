ffff218559fff5d1318535d6915f20b02a681cdc
github username <github email address>
2019-05-31 17:04:53 -0700

    add services folder with PostService.js that has some code to test api fetch requests

---
+++ src/services/PostService.js
@@ -0,0 +1,10 @@
+const API_URL = process.env.REACT_APP_API_URL;
+
+const PostService = {
+  fetchPosts() {
+    return fetch(`${API_URL}/posts`)
+      .then(r=>r.json())
+  };
+}
+
+export default PostService;
