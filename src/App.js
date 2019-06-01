f7c3ea3f97917c55fa56acde3c0f8b2fa8b11707
github username <github email address>
2019-05-31 18:55:29 -0700

    testing api requests

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 
+
 class App extends React.Component {
 
   componentDidMount() {
@@ -13,6 +14,7 @@ class App extends React.Component {
     return (
       <div>
         Hello World
+
       </div>
     );
   }
--- src/services/PostService.js
+++ src/services/PostService.js
@@ -1,10 +1,12 @@
 const API_URL = process.env.REACT_APP_API_URL;
 
-const PostService = {
+// Step One: fetch all the posts from the api
+// Step Two: 
+const PostService= {
   fetchPosts() {
     return fetch(`${API_URL}/posts`)
       .then(r=>r.json())
+    }
   };
-}
 
 export default PostService;
