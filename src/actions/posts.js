a9cbab52966d3546ce06c81f2c967d43c6fc271b
github username <github email address>
2019-05-31 20:27:33 -0700

    create state and action creator for posts fetch

--- src/actions/posts.js
+++ src/actions/posts.js
@@ -1,2 +1,30 @@
 import PostService from '../services/PostService';
-import { makeFetchRequest } from './appTransactions';
+import { makeFetchRequest, finishFetchRequest, unsuccessfulFetchRequest } from './appTransactions';
+
+// Synchronous Action Creators
+const successfullPostsFetch = posts => {
+  return {
+    type: 'SUCCESSFUL_IDEAS_FETCH',
+    posts: posts
+  }
+}
+
+// Async Action Creators
+export const fetchPosts = () => {
+  return dispatch => {
+          //(appTransactions)
+    dispatch(makeFetchRequest())
+    // API
+    PostService.fetchPosts()
+      // iterate through each post
+      .then(posts => {
+                //(appTransactions)
+        dispatch(finishFetchRequest())
+        dispatch(successfullPostsFetch(posts))
+      })
+      .catch(error => {
+                //(appTransactions)
+        dispatch(unsuccessfulFetchRequest())
+      })
+  }
+}
--- src/reducers/posts.js
+++ src/reducers/posts.js
@@ -2,10 +2,8 @@
 
 export default (state = [], action) => {
   switch (action.type) {
-    // fetch posts action creator
     case "SUCCESSFUL_POSTS_FETCH":
       return action.posts
-
       default:
         return state
   }
