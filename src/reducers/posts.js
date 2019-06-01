ad9b55373f3deae2a221999c07491ec53efa0d05
github username <github email address>
2019-05-31 19:44:29 -0700

    add comments to posts reducer

--- src/reducers/posts.js
+++ src/reducers/posts.js
@@ -1,5 +1,8 @@
+// create state and action type for posts
+
 export default (state = [], action) => {
   switch (action.type) {
+    // fetch posts action creator
     case "SUCCESSFUL_POSTS_FETCH":
       return action.posts
 
